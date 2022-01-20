4d8204d76d2d9cff0eec31550e1f393b12668a45
Pathomkorn Muttarak <pathomkorn@th.ibm.com>
2022-01-20 11:39:49 +0700

    Added the first tag

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region  = var.region
+  region = var.region
 }
 
 resource "aws_vpc" "hashicat" {
@@ -16,7 +16,7 @@ resource "aws_vpc" "hashicat" {
   enable_dns_hostnames = true
 
   tags = {
-    name = "${var.prefix}-vpc-${var.region}"
+    name        = "${var.prefix}-vpc-${var.region}"
     environment = "Production"
   }
 }
@@ -111,6 +111,9 @@ data "aws_ami" "ubuntu" {
 resource "aws_eip" "hashicat" {
   instance = aws_instance.hashicat.id
   vpc      = true
+  tags = {
+    Department = "devops"
+  }
 }
 
 resource "aws_eip_association" "hashicat" {
